cd1e5f4a341f126b8e3b3832fe54819c7df0a151
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-30 18:57:09 +0000

    chart container state created

--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 class ChartContainer extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            songs: [],
+            currentSong: null
+        }
+    }
+
     render() {
         return (
         <h1>I am a container</h1>
